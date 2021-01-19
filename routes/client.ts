66cb971762ef4db3ce8a02871f2789b6960ee55f
alexey ershkov <ershkov.alex2000@gmail.com>
2021-01-19 21:50:27 +0300

    client card done

--- routes/client.ts
+++ routes/client.ts
@@ -7,6 +7,11 @@ const router = Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}))
 
+router.get('/clients', async (req, res) => {
+    const clients = await ClientDbModel.find({});
+    res.send(clients);
+})
+
 router.post('/addClient', async (req, resp) => {
     const client = req.body
     const dbClient = new ClientDbModel(client)
